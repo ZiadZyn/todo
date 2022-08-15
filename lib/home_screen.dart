0630bad529f01b008067c082612f1570455aa8fc
islami <ziadzyn0@gmail.com>
2022-08-15 20:06:38 +0200

    finished splash screen,theming and locaization.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
+
 class HomeScreen extends StatelessWidget {
 static String routeName = 'home_screen';
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
 
     );
   }
---
+++ lib/my_theme.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyTheme{
+  static Color lightBlue=Color.fromRGBO(93, 156, 236, 1.0);
+  static Color lightGreen=Color.fromRGBO(97, 231, 87, 1.0);
+  static Color lightRed=Color.fromRGBO(236, 75, 75, 1.0);
+  static Color white=Color.fromRGBO(255, 255, 255, 1.0);
+  static Color black=Color.fromRGBO(56, 56, 56, 1.0);
+  static Color gray=Color.fromRGBO(200, 201, 203, 1.0);
+  static Color primaryGray=Color.fromRGBO(200, 201, 203, 1.0);
+  static final ThemeData lightMode = ThemeData(
+    primaryColor: lightBlue,
+    scaffoldBackgroundColor: primaryGray,
+    appBarTheme: AppBarTheme(
+      backgroundColor: lightBlue,
+      shadowColor: Colors.transparent,
+      centerTitle: true,
+    ),
+    textTheme: TextTheme(
+      headline4: TextStyle(
+        color: white,
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+      ),
+      headline5: TextStyle(
+        color: lightGreen,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      headline6: TextStyle(
+        color: lightBlue,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      subtitle1: TextStyle(
+        color: black,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+
+  static final ThemeData darkMode = ThemeData(
+    primaryColor: black,
+    scaffoldBackgroundColor: black,
+    appBarTheme: AppBarTheme(
+      backgroundColor: lightBlue,
+      shadowColor: Colors.transparent,
+      centerTitle: true,
+    ),
+    textTheme: TextTheme(
+      headline4: TextStyle(
+        color: black,
+        fontSize: 22,
+        fontWeight: FontWeight.bold,
+      ),
+      headline5: TextStyle(
+        color: lightGreen,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      headline6: TextStyle(
+        color: lightBlue,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      subtitle1: TextStyle(
+        color: white,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
